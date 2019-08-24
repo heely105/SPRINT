f611f5b31cc17b13b3bce0e4282063156e816558
chatyo <hanksistherevolution@naver.com>
2019-08-24 16:04:35 +0900

    omni

---
+++ Omni/motor2.py
@@ -0,0 +1,30 @@
+
+def cw(num):
+
+	from dynamixel_helper import DxlHelper
+	helper = DxlHelper("preset2.json", verbosity="detailed")
+	motor = helper.get_motor(2)
+	dxl_unit, res = motor.get_present_position()
+	motor.set_torque(False)
+	motor.set_operating_mode(1)
+	motor.set_torque(True)
+
+	if num == 'stop':
+		motor.set_torque(False)
+	elif num == 'go':
+		print(motor.set_goal_velocity(-200))
+	
+def ccw(num):
+	from dynamixel_helper import DxlHelper
+	helper = DxlHelper("preset2.json", verbosity="detailed")
+	motor = helper.get_motor(2)
+	dxl_unit, res = motor.get_present_position()
+	motor.set_torque(False)
+	motor.set_operating_mode(1)
+	motor.set_torque(True)
+
+	if num == 'stop':
+		motor.set_torque(False)
+	elif num == 'go':
+		print(motor.set_goal_velocity(200))
+        
