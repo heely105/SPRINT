219d96cffb0f986acd69a3491695f816f22ca861
Hong-ryul Jung <jung.hr.1206@gmail.com>
2019-10-27 00:24:33 +0900

    Fix error

---
+++ SHero_turtlebot3/include/turtlebot3/turtlebot3.h
@@ -0,0 +1,6 @@
+// #include "turtlebot3_motor_driver.h"
+#include "SHero_turtlebot3_omni_motor_driver.h"
+
+#include "turtlebot3_sensor.h"
+#include "turtlebot3_controller.h"
+#include "turtlebot3_diagnosis.h"
